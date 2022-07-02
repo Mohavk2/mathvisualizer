efbd0e4bc21d350df912cab8c6b9c6f40d3d2273
Mohavk2 <mohavkv12@gmail.com>
2022-07-03 01:57:16 +0300

    Add. Init layout

---
+++ src/pages/Main.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Sidebar from "../components/Sidebar"
+
+function Main() {
+    return (
+        <>
+            <Header/>
+            <div>Main page</div>
+            <Sidebar/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Main
